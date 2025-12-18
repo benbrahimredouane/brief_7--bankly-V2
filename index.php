6bd8e03b97c2b01aedda54f3ba2fbf68c7e18752
redouane <benbrahimredouane664@gmail.com>
2025-12-18 14:54:14 +0100

    feat(index.php):add index page

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Bankly V2 - Connexion</title>
+</head>
+<body>
+    <h2>Connexion des Employés</h2>
+    <form method="POST">
+        <label>Utilisateur :</label><br>
+        <input type="text" name="name" required><br><br>
+        <label>Mot de passe :</label><br>
+        <input type="password" name="password" required><br><br>
+        <button type="submit" name="connexion">Se connecter</button>
+    </form>
+
+</body>
+</html>
