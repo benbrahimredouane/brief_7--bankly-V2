977885fd03b2ae4550d818e83dc24e8d0c1a1b65
redouane <benbrahimredouane664@gmail.com>
2025-12-29 00:20:52 +0100

    chore(list_clients.php):remove transaction from nav bar make it under it with smoth style + add edit link

--- list_clients.php
+++ list_clients.php
@@ -12,7 +12,7 @@ $result = mysqli_query($conn, "SELECT * FROM clients");
 <html lang="fr">
 
 <head>
-    <title>Liste des Clients</title>
+    <title>Liste des Clients</title> 
     <style>
         body {
             font-family: 'Segoe UI', Arial, sans-serif;
@@ -65,6 +65,24 @@ $result = mysqli_query($conn, "SELECT * FROM clients");
             text-transform: uppercase;
             font-size: 13px;
         }
+        .btns{
+            display: flex;
+            gap:5px;
+        }
+        .list{
+            text-align: center;
+        }
+        .link{
+            display: flex;
+            justify-content: center;
+            text-decoration: none;
+            color: #ffffffff;
+            padding: 10px;
+            background-color: #007bffff;
+        }
+        .link:hover{
+            background: #6092c6ff;
+        }
     </style>
 </head>
 
@@ -73,13 +91,13 @@ $result = mysqli_query($conn, "SELECT * FROM clients");
         <a href="dashboard.php">Dashboard</a>
         <a href="list_clients.php"> Clients</a>
         <a href="list_accounts.php"> Comptes</a>
-        <a href="make_transaction.php"> Transaction</a>
         <a href="transactions_history.php"> Historique</a>
         <a href="logout.php" style="color:red"> Déconnexion</a>
     </nav>
     <hr>
-    <h2>Liste des Clients</h2>
-    <table border="1">
+    <h2 class="list">Liste des Clients</h2>
+    <a class="link" href="add_client.php">add client</a>
+    <table>
         <tr>
             <th>ID</th>
             <th>Nom</th>
@@ -93,10 +111,14 @@ $result = mysqli_query($conn, "SELECT * FROM clients");
                 <td><?php echo $row['name']; ?></td>
                 <td><?php echo $row['email']; ?></td>
                 <td><?php echo $row['cin']; ?></td>
-                <td>
+                <td class="btns">
                     <a href="delete_client.php?id=<?php echo $row['client_id']; ?>" style="color:red; font-weight:bold; text-decoration:none;">
                         Supprimer
                     </a>
+                     <a href="edit_client.php?id=<?php echo $row['client_id']; ?>"
+                            style="color:green; font-weight:bold; text-decoration:none; margin-right:10px;">
+                            Modifier
+                        </a>
                 </td>
             </tr>
         <?php endwhile; ?>
